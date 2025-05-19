5b009c0c2cf327b950f1e0d02995b7029d7f54cd
bhumilalwani <lalwanibhumi91@gmail.com>
2025-05-20 00:29:32 +0530

    4th

--- src/app/components/ARViewer.js
+++ src/app/components/ARViewer.js
@@ -23,22 +23,22 @@ export default function ARViewer() {
         ></a-asset-item>
       </a-assets>
 
-      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+      <a-camera position="0.5 0.5 0" scale="0.01 0.01 0.1" look-controls="enabled: false"></a-camera>
 
       <a-entity mindar-image-target="targetIndex: 0">
-        <a-plane
+        {/* <a-plane
           src="#card"
           position="0 0 0"
           height="0.552"
           width="1"
           rotation="0 0 0"
-        ></a-plane>
+        ></a-plane> */}
         <a-gltf-model
           rotation="0 0 0"
-          position="0 0 0.1"
-          scale="0.005 0.005 0.005"
+          position="0 0 0.5"
+          scale="0.2 0.2 0.2"
           src="#avatarModel"
-          animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
+
         ></a-gltf-model>
       </a-entity>
     </a-scene>
